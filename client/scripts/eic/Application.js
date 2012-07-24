158643152464fa2a201b33689890eff616129074
Ruben Verborgh <ruben.verborgh@gmail.com>
2012-07-24 15:16:19 +0200

    Added start button.

--- client/scripts/eic/Application.js
+++ client/scripts/eic/Application.js
@@ -19,8 +19,16 @@ function ($) {
       }, 100);
     },
     
+    updateMovieStatus: function () {
+      var topic = $('#topic').val(),
+          valid = topic.trim().length > 0;
+      $('.step.three')[valid ? 'removeClass' : 'addClass']('inactive');
+      $('#start').prop('disabled', !valid);
+    },
+    
     attachEventHandlers: function () {
       $('#facebook-connect').click($.proxy(this, 'connectToFacebook'));
+      $('#topic').on('change keyup', $.proxy(this, 'updateMovieStatus'));
     },
   };
   
