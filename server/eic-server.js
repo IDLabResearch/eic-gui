7435fb386e08dccd99405b9a3d8ef9c4381c2e05
Ruben Verborgh <ruben.verborgh@gmail.com>
2012-07-24 12:53:40 +0200

    Added main LESS stylesheet.

--- server/eic-server.js
+++ server/eic-server.js
@@ -1,5 +1,6 @@
 "use strict";
 var express = require('express'),
+    less = require('less'),
     fs = require('fs');
 
 var app = module.exports = express.createServer();
@@ -11,6 +12,19 @@ app.start = function (port, staticFolder) {
   this.listen(port);
   console.log('Everything Is Connected server running at http://localhost:' + port + '/');
 };
+
+app.engine('.less', function (path, options, callback) {
+  fs.readFile(path, 'utf8', function (error, contents) {
+    if (error)
+      return callback(error);
+    less.render(contents, { paths: [ app.staticFolder + '/stylesheets' ] }, callback);
+  });
+});
+
+app.get(/^\/stylesheets\/(?:\w+)$/, function (req, res) {
+  res.contentType('text/css');
+  res.render(app.staticFolder + req.url + '.less');
+});
 
 app.get(/^\/(?:\w+\/)*(?:\w+\.\w+)?$/, function (req, res) {
   res.sendfile(app.staticFolder + req.url);
