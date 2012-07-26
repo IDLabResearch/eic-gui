7c100458330751c1535c6ecd750c6a176676da10
Laurens De Vocht <laurensdv@gmail.com>
2012-07-26 17:44:58 +0200

    combination of images 1st version

--- client/scripts/eic/generators/FBProfilePhotosGenerator.js
+++ client/scripts/eic/generators/FBProfilePhotosGenerator.js
@@ -2,6 +2,63 @@ define(['lib/jquery', 'eic/generators/BaseSlideGenerator'],
 function ($, BaseSlideGenerator) {
   "use strict";
 
+  
+  function combineImagesToCanvas(images, canvas) {
+    var numOfImages = images.length;
+    var canvas = document.createElement("canvas");
+    var context = canvas.getContext("2d");
+		var i=0,j=0;
+		
+    $.each(images, function(index, image) {
+      context.drawImage(image, i * 100, j * 100, 100, 100);
+      i++;
+      j++;
+      if (i % 5 === 0)
+        j++;
+      if (j == 5)
+        return;
+    });
+    
+    convertCanvasToImage(image);
+  }
+
+  
+
+  function loadImages(sources, canvas, callback) {
+    var images = {};
+    var loadedImages = 0;
+    var numImages = 0;
+    // get num of sources
+    for (var src in sources) {
+      numImages++;
+    }
+    for (var src in sources) {
+      images[src] = new Image();
+      images[src].onload = function() {
+        if (++loadedImages >= numImages) {
+          callback(images, canvas);
+        }
+      };
+      images[src].src = sources[src];
+    }
+  }
+
+  function convertImageToCanvas(image) {
+    var canvas = document.createElement("canvas");
+    canvas.width = image.width;
+    canvas.height = image.height;
+    canvas.getContext("2d").drawImage(image, 0, 0);
+
+    return canvas;
+  }
+
+  // Converts canvas to an image
+  function convertCanvasToImage(canvas) {
+    var image = new Image();
+    image.src = canvas.toDataURL("image/png");
+    return image;
+  }
+
   var defaultDuration = 1000;
 
   /** Generator of images slides from Facebook User Profile search results.
@@ -30,6 +87,7 @@ function ($, BaseSlideGenerator) {
     init: function () {
       if (this.inited)
         return;
+        
       var self = this;
       
       this.fbConnector.get('photos', function (response) {
@@ -38,14 +96,18 @@ function ($, BaseSlideGenerator) {
         });
       });
       
+      var myPlaces = new Array();
       this.fbConnector.findPlacesNearMe(function (response) {
-				$.each(response.data.slice(0, self.maxResults), function (number, place) {
+				$.each(response.data.slice(0, 25), function (number, place) {
           self.fbConnector.getPlace(place.id, function (response) {
-						self.addImageSlide(response.picture);
+						myPlaces.push(response.picture);
           });
           
         });
       });
+      
+      loadImages(myPlaces,combineImagesToCanvas);
+      
 
       this.inited = true;
     },
