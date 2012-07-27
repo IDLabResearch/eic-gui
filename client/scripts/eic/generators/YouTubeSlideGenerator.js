964c23e667b4ef0373c32a82aaae85be0a14c214
Ruben Verborgh <ruben.verborgh@gmail.com>
2012-07-27 14:15:23 +0200

    Disable preloading by default.

--- client/scripts/eic/generators/YouTubeSlideGenerator.js
+++ client/scripts/eic/generators/YouTubeSlideGenerator.js
@@ -3,7 +3,7 @@ function ($, BaseSlideGenerator) {
   "use strict";
   
   var playerCount = 0;
-  var preload = true;
+  var preload = false;
 
   /** Generator of YouTube videos using the YouTube API
    * The option parameter is a hash consisting of
