e386913dd2e11b43e451ff6d617da6add0becd57
Laurens De Vocht <laurensdv@gmail.com>
2012-10-02 14:50:11 +0200

    change pathfinder from local to pathfinding.restdesc.org

--- client/scripts/eic/PathFinder.js
+++ client/scripts/eic/PathFinder.js
@@ -9,8 +9,7 @@ function ($, EventEmitter) {
 	PathFinder.prototype = {
     findSubject : function (object_value, type, callback) {
 			$.ajax({
-					//TODO: Change to pathfinding.restdesc.org
-          url: "http://localhost:1112/findSubject?",
+          url: "http://pathfinding.restdesc.org/findSubject?",
           dataType: "jsonp",
           data: {
             object_value: object_value,
