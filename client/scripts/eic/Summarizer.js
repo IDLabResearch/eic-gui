da45a2fa1ee7e478ab45687a2b6b72eccb62c293
Miel VanderSande <miel.vandersande@ugent.be>
2012-10-25 16:51:18 +0200

    JSHint

--- client/scripts/eic/Summarizer.js
+++ client/scripts/eic/Summarizer.js
@@ -28,12 +28,12 @@ define(['lib/jquery', 'config/URLs'], function ($, urls) {
           var glue = '';
           var sentence = result.links[i - 1][Math.round(Math.random())];
           switch (sentence.type) {
-            case 'direct':
-              glue = result.topics[i - 1].topic.label + sentence.value + result.topics[i].topic.label + '. ';
-              break;
-            case 'indirect':
-              glue = result.topics[i].topic.label + sentence.value + result.topics[i - 1].topic.label + '. ';
-              break;
+          case 'direct':
+            glue = result.topics[i - 1].topic.label + sentence.value + result.topics[i].topic.label + '. ';
+            break;
+          case 'indirect':
+            glue = result.topics[i].topic.label + sentence.value + result.topics[i - 1].topic.label + '. ';
+            break;
           }
           result.topics[i].text = glue + result.topics[i].text;
         }
@@ -56,26 +56,25 @@ define(['lib/jquery', 'config/URLs'], function ($, urls) {
           }
 
           var sentence = [
-          {
-            type: 'direct',
-            value: '\'s ' + decodeURIComponent(parts.join(' ').toLowerCase()) + ' is '
-          },
-          {
-            type: 'indirect',
-            value: '\'s the ' + decodeURIComponent(parts.join(' ').toLowerCase()) + ' of '
-          }
+            {
+              type: 'direct',
+              value: '\'s ' + decodeURIComponent(parts.join(' ').toLowerCase()) + ' is '
+            },
+            {
+              type: 'indirect',
+              value: '\'s the ' + decodeURIComponent(parts.join(' ').toLowerCase()) + ' of '
+            }
           ];
         
           self.result.links[index] = sentence;
 
 
-          if ((self.result.topics.length  == paths.vertices.length) 
-            && (self.result.links.length == paths.edges.length)) {
+          if ((self.result.topics.length  == paths.vertices.length) && (self.result.links.length == paths.edges.length)) {
             $(self).trigger('generated', formatResult(self.result));
           }
 
           console.log('Property: ' + property);
-          console.log('Generated sentence '+index+': ' + self.result.links[index][0].value);
+          console.log('Generated sentence ' + index + ': ' + self.result.links[index][0].value);
         }
         
         $(edges).each(retrieveTranscription);
@@ -93,16 +92,17 @@ define(['lib/jquery', 'config/URLs'], function ($, urls) {
             if (abstracts.length === 0)
               console.log('No abstracts found!');
             
-            function retrieveAbstract(index, vertice){
+            function retrieveAbstract(index, vertice) {
               var tregex = /\n|([^\r\n.!?]+([.!?]+|$))/gim;
               var sentences = abstracts[vertice]['abstract'].match(tregex);
               var desc = '';
               
-              for (var j = 0;j<sentences.length;j++){
+              for (var j = 0;j < sentences.length; j++) {
                 desc += sentences[j];
                 if (j > 2)
                   break;
-              }              
+              }
+              
               self.result.topics[index] = {
                 topic : {
                   type: 'person',
@@ -111,16 +111,17 @@ define(['lib/jquery', 'config/URLs'], function ($, urls) {
                 text : desc
               };
       
-              if ((self.result.topics.length  == paths.vertices.length) 
-                && (self.result.links.length == paths.edges.length)) {
+              if ((self.result.topics.length  == paths.vertices.length) && (self.result.links.length == paths.edges.length)) {
                 $(self).trigger('generated', formatResult(self.result));
               }
+              
+              console.log('Resource: ' + vertice);
+              console.log('Extracted text: ' + desc);
             }
             
             $(vertices).each(retrieveAbstract);
 
-            console.log('Resource: ' + vertice);
-            console.log('Extracted text: ' + desc);
+            
           },
           error: function (err) {
             console.log('Error retrieving abstracts: ' + err);
