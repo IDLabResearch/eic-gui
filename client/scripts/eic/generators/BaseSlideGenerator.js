7e672b60cb0be7db8704e6d326e1f96430f42f5a
Ruben Verborgh <ruben.verborgh@gmail.com>
2012-11-05 11:34:14 +0100

    Cleanup ErrorSlideGenerator and BaseSlideGenerator.

--- client/scripts/eic/generators/BaseSlideGenerator.js
+++ client/scripts/eic/generators/BaseSlideGenerator.js
@@ -1,8 +1,6 @@
 define(['lib/jquery', 'lib/jvent'],
 function ($, EventEmitter) {
   "use strict";
-  
-  var defaultDuration = 1000;
 
   /** Generator that serves as a base for other generators. */
   function BaseSlideGenerator() {
@@ -12,20 +10,20 @@ function ($, EventEmitter) {
   BaseSlideGenerator.prototype = {
     /** Initialize the generator. */
     init: function () {},
-    
+
     /** Returns whether there any more. */
     hasNext: function () { return false; },
 
     /** Get the next slide. */
     next: function () { return null; },
-    
+
     /** Prepare the upcoming slides if applicable (not guaranteed to be called). */
     prepare: function () { },
-    
+
     /** Create a base slide witht the specified class, content, and duration. */
     createBaseSlide: function (cssClass, content, duration) {
       var slide = new EventEmitter();
-      
+
       // Create slide element.
       var $wrapper = $('<div>').addClass('inner');
       slide.$element = $('<div>').addClass('slide')
@@ -34,14 +32,14 @@ function ($, EventEmitter) {
 
       // Set duration.
       slide.duration = duration;
-      
+
       // Set start and stop functions.
       slide.start = $.proxy(slide, 'emit', 'started');
       slide.stop  = $.proxy(slide, 'emit', 'stopped');
-      
+
       return slide;
     },
   };
-  
+
   return BaseSlideGenerator;
 });
--- client/scripts/eic/generators/ErrorSlideGenerator.js
+++ client/scripts/eic/generators/ErrorSlideGenerator.js
@@ -2,17 +2,9 @@ define(['lib/jquery', 'eic/generators/BaseSlideGenerator'],
 function ($, BaseSlideGenerator) {
   "use strict";
 
-  /*
-   * EXTEND
-   * CLEANUP
-   **/
-
-  var defaultDuration = 10000;
-
   /** Generator that creates a title slide for a topic. */
   function ErrorSlideGenerator(errorText) {
     BaseSlideGenerator.call(this);
-    
     this.errorText = errorText;
   }
 
@@ -21,8 +13,8 @@ function ($, BaseSlideGenerator) {
   {
     /** Checks whether the title slide has been shown. */
     hasNext: function () {
-        return this.done !== true;
-      },
+      return this.done !== true;
+    },
 
     /** Advances to the title slide. */
     next: function () {
@@ -32,19 +24,16 @@ function ($, BaseSlideGenerator) {
         $text = $('<h1>').text(this.errorText),
         $link = $('<a>', {
           text: 'Try again',
-          title: 'reload',
           href: '#',
           click: function () { location.reload(); }
         });
-      $($content).append($text);
-      $($content).append($link);
-      var slide = this.createBaseSlide('error', $content, defaultDuration);
-      
+      $content.append($text, $link);
+      var slide = this.createBaseSlide('error', $content, 0);
       this.done = true;
 
       return slide;
     },
   });
-  
+
   return ErrorSlideGenerator;
 });
