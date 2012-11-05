ec8752555a81c6cb404152a34094a64fdc09e004
Ruben Verborgh <ruben.verborgh@gmail.com>
2012-11-05 10:16:22 +0100

    Prefer nodes with a minimum connectivity of 10.

--- client/scripts/eic/TopicSelector.js
+++ client/scripts/eic/TopicSelector.js
@@ -4,6 +4,9 @@ function ($, urls) {
 
   // The topic is selected from the top `topCandidates` most connected topics
   var topCandidates = 10;
+  // Preferred minimum connectivity for a node to be selected.
+  var minConnectivity = 10;
+  function hasLargeConnectivity(t) { return t.connectivity > minConnectivity; }
 
   // Topic selector with a user's Facebook profile as input
   function TopicSelector(facebookConnector) {
@@ -14,11 +17,8 @@ function ($, urls) {
     // Select a topic, based on the user's Facebook profile
     selectTopic: function (callback) {
       var self = this;
-      var likes = [];
       self.facebookConnector.get('likes', function (response) {
-        $.each(response.data, function (index, like) {
-          likes.push(like);
-        });
+        var likes = response.data;
         if (!likes.length)
           throw "This user has no likes, so no topic can be found.";
 
@@ -36,10 +36,12 @@ function ($, urls) {
             if (!topics.length)
               throw "None of the user's likes could be mapped to a topic.";
 
-            // Sort the topics by descending connectivity
-            topics = topics.filter(function (t) { return t.connectivity > 0; })
-                           .sort(function (a, b) { return b.connectivity - a.connectivity; });
+            // Use only strongly connected topics (if they exist)
+            if (topics.some(hasLargeConnectivity))
+              topics = topics.filter(hasLargeConnectivity);
+
             // Pick one of the most connected topics
+            topics = topics.sort(function (a, b) { return b.connectivity - a.connectivity; });
             var topic = topics[Math.floor(Math.random() *
                                           Math.min(topics.length, topCandidates))];
             callback(topic);
