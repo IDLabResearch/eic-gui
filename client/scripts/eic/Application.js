171f713a5fd2cf2e41a86bcbcb35ba98936cd1d2
Ruben Verborgh <ruben.verborgh@gmail.com>
2012-11-07 20:32:27 +0100

    Fixed JSHint validation.

--- client/scripts/eic/Application.js
+++ client/scripts/eic/Application.js
@@ -9,26 +9,26 @@ define(['lib/jquery', 'eic/AutocompleteTopic', 'eic/FacebookConnector',
     ErrorSlideGenerator,
     SlidePresenter, TopicSelector) {
     "use strict";
-  
-    function Application(){
-      this.width = 120;
-      
+
+    var pieceWidth = 120;
+
+    function Application() {
       this.facebookConnector = new FacebookConnector();
       this.topicSelector = new TopicSelector(this.facebookConnector);
       this.generator = new CombinedSlideGenerator();
     }
-  
+
     Application.prototype = {
-      init: function(){
+      init: function () {
         this.drawPieces($('#title_1'), 5, 'images/piece3.svg');
         this.drawPieces($('#title_2'), 1, 'images/piece2.svg');
         this.drawPieces($('#title_3'), 5, 'images/piece3.svg');
         this.drawPieces($('#play'), 1, 'images/piece1.svg', 100);
-        
+
         $('#frame').show();
-      
+
         this.drawBigPieces($('#steps'));
-    
+
         var self = this;
         this.initControls();
         this.facebookConnector.init();
@@ -46,18 +46,18 @@ define(['lib/jquery', 'eic/AutocompleteTopic', 'eic/FacebookConnector',
               self.generator.addGenerator(new ErrorSlideGenerator(error));
             });
         });
-    
+
       },
-      drawPieces: function($title, nr, img, fontsize) {
+      drawPieces: function ($title, nr, img, fontsize) {
         for (var i = 0; i < nr; i++) {
-          var x =  i * (this.width - 0.22 * this.width),
-          y = ((i % 2) * 0.215 * this.width);
+          var x =  i * (pieceWidth - 0.22 * pieceWidth),
+          y = ((i % 2) * 0.215 * pieceWidth);
 
           this.drawPiece($title, {
-            x: i, 
-            y: 0, 
-            size: this.width, 
-            scaleY: (1 - 2 * (i % 2)), 
+            x: i,
+            y: 0,
+            size: pieceWidth,
+            scaleY: (1 - 2 * (i % 2)),
             img: img
           });
 
@@ -65,20 +65,20 @@ define(['lib/jquery', 'eic/AutocompleteTopic', 'eic/FacebookConnector',
           var $content = $title
           .children('.content');
           $content.css({
-            'margin-top': fontsize ? (this.width - fontsize) / 2 + 10 : 10,
-            'margin-left': fontsize ? 30 : 0.22 * this.width,
-            'font-size':   fontsize || this.width
+            'margin-top': fontsize ? (pieceWidth - fontsize) / 2 + 10 : 10,
+            'margin-left': fontsize ? 30 : 0.22 * pieceWidth,
+            'font-size':   fontsize || pieceWidth
           });
         }
       },
       drawPiece: function ($elem, options) {
         var x =  options.x * (options.size - 0.22 * options.size) + ((1 - (options.y % 2)) * 0.215 * options.size),
         y = options.y * (options.size - 0.22 * options.size) + ((options.x % 2) * 0.215 * options.size);
-        
+
         $elem
         .width(x + options.size)
         .height(options.size);
-                    
+
         return $('<div />')
         .addClass('piece')
         .css({
@@ -94,23 +94,23 @@ define(['lib/jquery', 'eic/AutocompleteTopic', 'eic/FacebookConnector',
         .appendTo($elem.children('.pieces'));
       },
       drawBigPieces: function ($title) {
-        var new_width = this.width * 4;
+        var new_width = pieceWidth * 4;
         var step = 1;
-   
+
         for (var i = 0; i < 2; i++) {
           for (var j = 0; j < 2; j++) {
-         
+
             var piece = this.drawPiece($title, {
-              x: i, 
-              y: j, 
-              size: new_width, 
+              x: i,
+              y: j,
+              size: new_width,
               scaleX: (1 - 2 * (j % 2)),
-              scaleY: (1 - 2 * (i % 2)), 
-              img: (i == 0 && j == 1)?'images/piece4.svg':'images/piece3.svg'
+              scaleY: (1 - 2 * (i % 2)),
+              img: (i === 0 && j === 1) ? 'images/piece4.svg' : 'images/piece3.svg'
             })
             .attr('id', 'piece_' + step)
             .hide();
-            
+
             $('#step_' + step).css({
               display: 'none',
               left: piece.css('left'),
@@ -119,11 +119,11 @@ define(['lib/jquery', 'eic/AutocompleteTopic', 'eic/FacebookConnector',
               'margin-top': new_width * 0.22,
               width: new_width - 2 * (new_width * 0.22) - 10
             });
-           
+
             step++;
           }
         }
-        
+
         //Some fixes adjusting the last step
         $('#step_4').css({
           'margin-left': new_width * 0.215 + 40,
@@ -131,28 +131,28 @@ define(['lib/jquery', 'eic/AutocompleteTopic', 'eic/FacebookConnector',
           width: new_width - 2 * (new_width * 0.22) - 40
         });
       },
-      disableElement: function($elem, disabled) {
-        if (disabled) 
+      disableElement: function ($elem, disabled) {
+        if (disabled)
           $elem
           .addClass('disabled')
           .prop("disabled", true);
-        else 
+        else
           $elem
           .removeClass('disabled')
           .prop("disabled", false);
       },
-      animate: function ($elem, name, duration, callback){
+      animate: function ($elem, name, duration, callback) {
         if (callback)
-          window.setTimeout(callback, duration*1000);
-          
+          window.setTimeout(callback, duration * 1000);
+
         return $elem.css({
           '-webkit-animation-name': name,
           '-webkit-animation-duration': duration + 's',
           '-webkit-transition-timing-function': 'linear'
         });
       },
-      initControls: function(){
-        var new_width = this.width * 4;
+      initControls: function () {
+        var new_width = pieceWidth * 4;
         var self = this;
 
         $('<span />')
@@ -165,43 +165,43 @@ define(['lib/jquery', 'eic/AutocompleteTopic', 'eic/FacebookConnector',
         .appendTo($('#piece_1'))
         .click(function () {
           $('#step_1').hide();
-          
-          self.animate($('#piece_1'), 'rotatein', 0.3, 
+
+          self.animate($('#piece_1'), 'rotatein', 0.3,
             function () {
               $('#play').show();
               $('#piece_1').hide();
             });
         });
-        
+
         //Shows the stepsmenu
         $('#play').mouseover(function () {
           $(this).hide();
-          
-          self.animate($('#piece_1'), 'rotateout', 0.3, 
+
+          self.animate($('#piece_1'), 'rotateout', 0.3,
             function () {
               $('#step_1').show();
             })
           .show();
         });
-        
+
         $('#step_1 .next:not(.disabled)').live('click', function () {
           self.disableElement($('#step_1 .button, #step_1 input'), true);
           self.disableElement($('#step_3 .back'), false);
-         
-          self.animate($('#steps'),'rotate1to2',0.3)
+
+          self.animate($('#steps'), 'rotate1to2', 0.3)
           .css({
             'left': -200,
             'top': 250,
             '-webkit-transform': 'rotate(-95deg)'
           });
-          
+
           self.animate($('#piece_3, #step_3'), 'slide2', 0.5)
           .show();
         });
-        
+
         $('#step_3 .back:not(.disabled)').live('click', function () {
           self.disableElement($('#step_1 .button, #step_1 input'), false);
-          
+
           self.animate($('#steps'), 'rotate2to1', 0.3,
             function () {
               $('#piece_3, #step_3').hide();
@@ -211,7 +211,7 @@ define(['lib/jquery', 'eic/AutocompleteTopic', 'eic/FacebookConnector',
             'top': 0,
             '-webkit-transform': 'rotate(-5deg)'
           });
-          
+
           self.animate($('#piece_3, #step_3'), 'slide2rv', 0.5);
         });
 
@@ -219,23 +219,23 @@ define(['lib/jquery', 'eic/AutocompleteTopic', 'eic/FacebookConnector',
           function () {
             self.disableElement($('#step_3 .button, #step_3 input'), true);
             self.disableElement($('#step_4 .back'), false);
-            
+
             self.animate($('#steps'), 'rotate2to3', 0.3)
             .css({
               'left': 350,
               'top': -200,
               '-webkit-transform': 'rotate(95deg)'
             });
-          
-            self.animate($('#piece_4, #step_4'), 'slide3',0.5)
+
+            self.animate($('#piece_4, #step_4'), 'slide3', 0.5)
             .show();
           });
-        
+
         $('#step_4 .back:not(.disabled)').live('click',
           function () {
             self.disableElement($('#step_3 .button, #step_3 input'), false);
-            
-            self.animate($('#steps'), 'rotate3to2', 0.3, 
+
+            self.animate($('#steps'), 'rotate3to2', 0.3,
               function () {
                 $('#piece_4, #step_4').hide();
               })
@@ -244,19 +244,19 @@ define(['lib/jquery', 'eic/AutocompleteTopic', 'eic/FacebookConnector',
               'top': 250,
               '-webkit-transform': 'rotate(-95deg)'
             });
-          
-            self.animate($('#piece_4, #step_4'), 'slide3rv', 0.5);         
+
+            self.animate($('#piece_4, #step_4'), 'slide3rv', 0.5);
           });
-        
+
         $('.play_button').click(function () {
           self.drawScreen($('#screen'));
         });
-       
+
         var fbContent = $('#facebook').html();
 
         // Initialize the controls of each step.
         $('#facebook-connect').live('click', $.proxy(this, 'connectToFacebook'));
-        
+
         $('#begintopic').on('change keyup', $.proxy(this, 'updateBeginTopic'));
         $('#endtopic').on('change keyup', $.proxy(this, 'updateEndTopic'));
 
@@ -281,7 +281,7 @@ define(['lib/jquery', 'eic/AutocompleteTopic', 'eic/FacebookConnector',
               click: function () {
                 self.facebookConnector.disconnect(
                   //$.proxy(window.location, 'reload')
-                  $.proxy(function(){
+                  $.proxy(function () {
                     $('#begintopic').show();
                     self.disableElement($('#step_1 .next'), true);
                     $('#facebook').html(fbContent);
@@ -326,11 +326,11 @@ define(['lib/jquery', 'eic/AutocompleteTopic', 'eic/FacebookConnector',
       drawScreen: function ($screen) {
         var self = this;
         $screen.show();
-                
+
         this.animate($('#piece_2'), 'drop', 0.3)
         .show();
-        
-        this.animate($('#steps'),'moveToScreen', 0.7,
+
+        this.animate($('#steps'), 'moveToScreen', 0.7,
           function () {
             $('#frame').remove();
             self.playMovie();
@@ -338,8 +338,8 @@ define(['lib/jquery', 'eic/AutocompleteTopic', 'eic/FacebookConnector',
         .css({
           width: '0px',
           height: '0px',
-          left: -3 * 0.88 * this.width - 5,
-          top:  400 - (this.width * 8 * 2),
+          left: -3 * 0.88 * pieceWidth - 5,
+          top:  400 - (pieceWidth * 8 * 2),
           '-webkit-transform': 'rotate(0deg) scale(3, 3)'
         });
       },
@@ -359,7 +359,7 @@ define(['lib/jquery', 'eic/AutocompleteTopic', 'eic/FacebookConnector',
 
         if (this.intro)
           this.generator.addGenerator(this.intro);
-        
+
         this.topicToTopic = new TopicToTopicSlideGenerator(this.beginTopic);
         this.generator.addGenerator(this.topicToTopic);
         this.topicToTopic.setEndTopic(this.endTopic);
@@ -370,7 +370,7 @@ define(['lib/jquery', 'eic/AutocompleteTopic', 'eic/FacebookConnector',
         var presenter = new SlidePresenter($slides, this.generator, $audio);
         presenter.start();
       }
-    }
+    };
 
     return Application;
   });
