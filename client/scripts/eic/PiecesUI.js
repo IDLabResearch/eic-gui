08cbc615134b338256717a925b51d909ea0a4521
Ruben Verborgh <ruben@verborgh.org>
2013-01-02 10:38:39 +0100

    Remove unnecessary dependencies from PiecesUI.

--- client/scripts/eic/PiecesUI.js
+++ client/scripts/eic/PiecesUI.js
@@ -1,13 +1,5 @@
-define(['lib/jquery', 'eic/AutocompleteTopic', 'eic/DrawPiece', 'eic/FacebookConnector',
-  'eic/generators/IntroductionSlideGenerator', 'eic/generators/OutroductionSlideGenerator',
-  'eic/generators/TopicToTopicSlideGenerator', 'eic/generators/CombinedSlideGenerator',
-  'eic/generators/ErrorSlideGenerator',
-  'eic/SlidePresenter', 'eic/TopicSelector'],
-  function ($, autocompleteTopic, drawPiece, FacebookConnector,
-    IntroductionSlideGenerator, OutroductionSlideGenerator,
-    TopicToTopicSlideGenerator, CombinedSlideGenerator,
-    ErrorSlideGenerator,
-    SlidePresenter, TopicSelector) {
+define(['lib/jquery', 'eic/AutocompleteTopic', 'eic/DrawPiece'],
+  function ($, autocompleteTopic, drawPiece) {
     "use strict";
 
     var pieceWidth = 120;
